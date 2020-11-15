28eb5d94e707087d51567898851058891b713c2a
Rajiv Krishnan <rajivkris@gmail.com>
2020-11-15 15:19:36 +0530

    Dijkstra Algo implementation

---
+++ Search Algo/Dijkstras_Algo.py
@@ -0,0 +1,93 @@
+from collections import defaultdict
+import sys
+class Graph:
+    def __init__(self):
+        self.nodes = set()                   # A set cannot contain duplicate nodes
+        self.neighbours = defaultdict(list)  # Defaultdict is a child class of Dictionary that provides a default value for a key that does not exists.
+        self.distances = {}                  # Dictionary. An example record as ('A', 'B'): 6 shows the distance between 'A' to 'B' is 6 units
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.neighbours[from_node].append(to_node)
+        self.neighbours[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
+        self.distances[(to_node, from_node)] = distance    # lets make the graph undirected / bidirectional
+       
+    def print_graph(self):
+        print("Set of Nodes are: ", self.nodes)
+        print("Neighbours are: ", self.neighbours)
+        print("Distances are: ", self.distances)
+
+def dijkstra(graph: Graph, source):
+    unvisited = graph.nodes
+    result = {}
+    path = {}
+    result = {i : sys.maxsize for i in graph.nodes}
+    result[source] = 0
+    min_value = 0
+    min_node = source
+    unvisited.remove(source)
+    while unvisited:
+        min_value = sys.maxsize
+        neighbours = graph.neighbours[min_node]
+
+        for i in neighbours:
+            if graph.distances[(min_node, i)] + result[min_node] < result[i]:
+                result[i] = graph.distances[(min_node, i)] + result[min_node]
+                path[i] = min_node
+
+        for i in unvisited:
+            if result[i] < min_value:
+                min_node = i
+                min_value = result[i]
+
+        unvisited.remove(min_node)
+    return result
+
+
+# Test 1
+testGraph = Graph()
+for node in ['A', 'B', 'C', 'D', 'E']:
+    testGraph.add_node(node)
+
+testGraph.add_edge('A','B',3)
+testGraph.add_edge('A','D',2)
+testGraph.add_edge('B','D',4)
+testGraph.add_edge('B','E',6)
+testGraph.add_edge('B','C',1)
+testGraph.add_edge('C','E',2)
+testGraph.add_edge('E','D',1)
+
+print(dijkstra(testGraph, 'A'))     # {'A': 0, 'D': 2, 'B': 3, 'E': 3, 'C': 4}
+
+
+# Test 2
+graph = Graph()
+for node in ['A', 'B', 'C']:
+    graph.add_node(node)
+   
+graph.add_edge('A', 'B', 5)
+graph.add_edge('B', 'C', 5)
+graph.add_edge('A', 'C', 10)
+
+print(dijkstra(graph, 'A'))        # {'A': 0, 'C': 10, 'B': 5}
+
+
+# Test 3
+graph = Graph()
+for node in ['A', 'B', 'C', 'D', 'E', 'F']:
+    graph.add_node(node)
+   
+graph.add_edge('A', 'B', 5)
+graph.add_edge('A', 'C', 4)
+graph.add_edge('D', 'C', 1)
+graph.add_edge('B', 'C', 2)
+graph.add_edge('A', 'D', 2)
+graph.add_edge('B', 'F', 2)
+graph.add_edge('C', 'F', 3)
+graph.add_edge('E', 'F', 2)
+graph.add_edge('C', 'E', 1)
+
+print(dijkstra(graph, 'A'))       # {'A': 0, 'C': 3, 'B': 5, 'E': 4, 'D': 2, 'F': 6}
